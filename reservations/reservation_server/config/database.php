59a38fcfe3a4ffa7cf6fd454c7a0323e93cf7ceb
Raveena Mattu <Raveena.Mattu@triosstudent.com>
2025-08-20 12:03:06 -0400

    Added server folder with database, config and api files

---
+++ reservations/reservation_server/config/database.php
@@ -0,0 +1,18 @@
+<?php
+
+  // Database configuration
+  $dbHost = 'localhost';
+  $dbUsername = 'root';
+  $dbName = 'react_reservations';
+  $dbPassword = '';
+
+  // Create a connection
+  $conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: ". $conn->connect_error);
+  } else {
+    echo "Connected successfully";
+  }
+?>
